c4ae609d0c9be079f345b5cfad28d6abf12581b1
esther <esthercar@leader.codes>
2021-06-08 14:01:17 +0300

    design components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Body from './components/chat/body'
+import './App.css';
+import React from 'react'
+
+const App = () => {
   return (
-    <div className="App">
+
+    <div className="App" style={{ backgroundColor: "black", height: "700px" }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Body/>
       </header>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/components/chat/myChats.js
@@ -0,0 +1,52 @@
+
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './chat.css'
+// import Image from 'react-bootstrap/Image'
+import profil from '../../assets/user.png'
+import ListGroup from 'react-bootstrap/ListGroup';
+// import chevron-left from '../../assets/user.png'
+// import chevronLeft from "../../assets/chevron-left.png"
+
+
+// זה יצטרך לבוא מהרידקס
+let members = ['Aviv', 'Shay', 'Oded']
+let messages = ['Thanks,', 'What a nice lecture!', 'hoooo']
+
+const myChats = () => { 
+
+    return (
+        <div className="chatBox">
+            <Card border="light" style={{ width: '19rem', minheight: '19rem' }}>
+
+
+                <Card.Title className="cardTitle row" style={{ color: '#D10010' }}>
+                    <div className="col-12"> My Chats</div>
+                </Card.Title>
+                <div className="linear" > </div>
+                <div className="container-fluid">
+                    <Card.Body>
+                        {members.map((member, index) => (
+                            <div className='row d-flex flex-row'>
+                                <img src={profil} className=' col-4 profil-img' rounded ></img>
+                                <div className='col-8'>
+                                    <div><b>{member}</b></div>
+                                    <div>  {messages[index]}</div>
+
+                                </div>
+                            </div>
+                        ))}
+                    </Card.Body>
+                    <div className="mb-2">
+                        <input className="form-control" type="text" placeholder="message" className="chatMessage"></input>
+                   {/* <img src={chevronLeft}></img> */}
+                    </div>
+
+                </div>
+            </Card >
+        </div>
+    )
+}
+
+export default myChats;
