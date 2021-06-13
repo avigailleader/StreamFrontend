d190885a994e5b33261463bfc1d32aa0232bb7dd
esther <esthercar@leader.codes>
2021-06-13 10:59:00 +0300

    installing bootstrap

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import './App.css';
 import React from 'react'
 import { Provider } from 'react-redux';
 import store from './redux/store'
+import {Button} from 'react-bootstrap'
+// import Button from 'react-bootstrap/Button'
 import StartVideo from './components/startVideo'
+
+ 
 const App = () => {
   return (
+
     <Provider store={store}>
 
-      <div className="App" style={{ backgroundColor: "black", height: "700px" }}>
+      <div className="App"  style={{ backgroundColor: "gray", height: "700px" }}>
+        <Button className="btn-primary">ghjg</Button>
         <header className="App-header">
           <Body />
-          <StartVideo></StartVideo>
+          <StartVideo style={{position: "absolute", right: "0px"}}></StartVideo>
 
         </header>
       </div>
