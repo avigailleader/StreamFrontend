8309d4bd63babe0b016cfda2ee697922daa40ccb
esther <esthercar@leader.codes>
2021-06-21 11:46:25 +0300

    saved

--- src/App.js
+++ src/App.js
@@ -2,19 +2,8 @@ import './App.css';
 import React from 'react'
 import { Provider } from 'react-redux';
 import store from './redux/store'
-<<<<<<< HEAD
-import Routes from './components/route'
 import { Button } from 'react-bootstrap'
-
-=======
-import { Button } from 'react-bootstrap'
-// import Button from 'react-bootstrap/Button'
-import StartVideo from './components/stream'
-import Chat from './components/chat/chatUser'
-import ChatAdmin from './components/chat/chatAdmin'
-
 import Routes from './components/routes/route'
->>>>>>> 84b35988e05d13765704c5bf33ff3bfd7ee88688
 
 const App = () => {
   return (
