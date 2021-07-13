e35aaf6c9e8c083cf3b4cc2730a38f07fa9c0e18
dassy-yehuda <dassy@leader.codes>
2021-07-13 11:05:37 +0300

    afterbig

--- src/components/video/afterVideo/afterVideo.js
+++ src/components/video/afterVideo/afterVideo.js
@@ -3,23 +3,37 @@ import { useDispatch, useSelector } from "react-redux";
 import { actions } from "../../../redux/actions/action"
 
 
-const AfterVideo=(props)=>{
+const AfterVideo = (props) => {
     const dispatch = useDispatch()
-    const url= useSelector(state => state.convarsetionReducer.url)
-    const videoLiveName=useSelector(state => state.convarsetionReducer.videoLiveName)
-    const [videoName,setVideoName]=useState(videoLiveName)
-   const  updatename=(val)=>{
-    setVideoName(val)
-    dispatch(actions.setVideoLiveName(videoName))
-   }
-  
- return(<>
- <div>AfterVideo work</div>
- <video id="localVideo" height="100%" width="100%" muted={true} autoPlay src={url} controls >
+    const url = useSelector(state => state.convarsetionReducer.url)
+    const videoLiveName = useSelector(state => state.convarsetionReducer.videoLiveName)
+    const [videoName, setVideoName] = useState(videoLiveName)
+    const updatename = (val) => {
+        setVideoName(val)
+        dispatch(actions.setVideoLiveName(videoName))
+    }
 
-</video>
-<div></div>
- <input type="text" placeholder="upDate name video" onChange={(e)=>updatename(e.target.value)}  value={videoName}></input></>)
+    return (<>
+<div className="container">
+    <div className="row">
+        <div className="col-2"></div>
+        <div className="col-8">
+        <div className="diVideo1">
+            <video controls className="localVideo1" height="100%" width="100%" autoPlay src={url}  >
+
+            </video>
+            <p className="myLabel">update name video</p>
+            <input type="text" placeholder="upDate name video" onChange={(e) => updatename(e.target.value)} value={videoName}></input>
+        </div>
+        </div>
+        <div className="col-2"></div>
+
+    </div>
+</div>
+
+       
+        <div></div>
+</>)
 
 }
 export default AfterVideo
