6ce2914902dc3803c4154de7a56266787d0e7494
avigail luria <avigail@avigails-Air.Dlink>
2021-07-22 15:38:53 +0300

    style

--- src/components/video/afterVideo/afterVideo.js
+++ src/components/video/afterVideo/afterVideo.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from "react-redux";
 import { actions } from "../../../redux/actions/action"
-
+import './afterVideo.css'
 
 const AfterVideo = (props) => {
     const dispatch = useDispatch()
@@ -9,37 +9,38 @@ const AfterVideo = (props) => {
     const videoLiveName = useSelector(state => state.convarsetionReducer.videoLiveName)
     const [videoName, setVideoName] = useState(videoLiveName)
     const [description, setDescription] = useState(useSelector(state => state.convarsetionReducer.description))
-   
-    
+
+
     const saveNameVideo = (e) => {
         debugger
         dispatch(actions.setVideoLiveName(videoName))
         dispatch(actions.setVideoLiveDescription(description))
-       
+
         dispatch(actions.saveVideo())
-            alert("save is sucsess")
-       
+        alert("save is sucsess")
+
 
     }
     return (<>
-        <div className="container">
+        <div className="container" className="whiteBackground">
             <div className="row">
-                <div className="col-2"></div>
-                <div className="col-8 ">
-                    <div className="diVideo1">
+                <div className="col-12 title">Video Editor</div></div>
+            <div className="row">
+                <div className="col-3"></div>
+                <div className="col-6 ">
+                    <div >
                         <video controls className="localVideo1" height="100%" width="100%" autoPlay src={url}  >
                         </video>
-                        <p className="myLabel">update name video</p>
-                        <input type="text" placeholder="upDate name video"  onChange={(e) => setVideoName(e.target.value)} value={videoName}></input>
-                        <p className="myLabel">description your video</p>
-                        <div class="form-group">
-                            <label for="comment">UpDate description video:</label>
-                            <textarea class="form-control textBox" rows="5" id="comment" onChange={(e) => setDescription(e.target.value)} value={description}></textarea>
-                            <button class="btn btn-primary" onClick={saveNameVideo}></button>
+                        <input className="videoNameInput" type="text" placeholder="Video Name" onChange={(e) => setVideoName(e.target.value)}></input>
+                        <div className="videoForm form-group">
+                            <textarea placeholder="Lesson Description" className=" videoNameInput " rows="5" id="comment" onChange={(e) => setDescription(e.target.value)}></textarea>
+                            <button className="whiteButton">cancel</button>
+                            <button className="redButton" onClick={saveNameVideo}>save</button>
+
                         </div>
                     </div>
                 </div>
-                <div className="col-2"></div>
+                <div className="col-3"></div>
 
             </div>
         </div>
