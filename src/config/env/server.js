25bcaae382b2a00bff3cba86ad8e6f2604d4fd89
avigail luria <avigail@avigails-Air.Dlink>
2021-07-25 10:30:31 +0300

    fix err in server config

--- src/config/env/server.js
+++ src/config/env/server.js
@@ -1,7 +1,7 @@
 const isDev = window.location.href.includes('dev.')
 
 module.exports = {
-    BASE_URL: isDev ? "https://stream.dev.vlogger.codes/" : "https://stream.vlogger.codes/",
+    BASE_URL: "https://stream.vlogger.codes/",
     CHAT_URL: isDev ? 'https://chat.dev.leader.codes/' : 'https://chat.leader.codes/',
     FILE_URL: isDev ? 'https://dev.files.codes/api/' : 'https://files.codes/api/'
 
